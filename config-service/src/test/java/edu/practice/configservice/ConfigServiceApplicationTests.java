434bc74e9e134f8e1a3363d0dd99ba8dba76b550
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2020-03-28 18:09:49 +0200

    config service created

---
+++ config-service/src/test/java/edu/practice/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.practice.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
