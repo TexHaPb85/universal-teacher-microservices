afd5bd912ed88923d28bca6c3aead0494ef2be1d
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2020-03-28 20:50:34 +0200

    registry service created

---
+++ registry-service/src/test/java/edu/practice/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.practice.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
